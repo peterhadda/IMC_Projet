5382e59c2c7c90bb4dde01f54b290ebb473783c5
peterhadda <104314694+peterhadda@users.noreply.github.com>
2026-01-14 08:26:03 -0800

    Update SauvegardeIMC.py

--- SauvegardeIMC.py
+++ SauvegardeIMC.py
@@ -1,4 +1,7 @@
 import sqlite3
+from pathlib import Path
+
+
 class SauvegardeIMC:
 
     def __init__(self,data):
@@ -9,6 +12,38 @@ class SauvegardeIMC:
         with open("data.txt","a",encoding="utf-8") as file:
             ligne=" | ".join(self.data)
             file.write(ligne+"\n")
+
+    def sauvegarder_dataIMC(self,ts, taille, poids, results_imc, classification, risque):
+        BASE_DIR = Path(__file__).resolve().parent
+
+        DATA_DIR = BASE_DIR / "data"
+
+        DB_DIR = DATA_DIR / "data_imc.db"
+
+        conn = sqlite3.connect(DB_DIR)
+        cursor = conn.cursor()
+
+        cursor.execute(
+            '''
+            CREATE TABLE IF NOT EXISTS data_imc(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            ts TEXT,
+            taille TEXT,
+            poids TEXT,
+            resultats TEXT,
+            classification TEXT,
+            risque TEXT)
+            '''
+        )
+        cursor.execute('''
+                  INSERT INTO data_imc(ts, taille, poids,resultats ,classification,risque) VALUES (?,?,?,?,?,?)
+                  ''', (ts, taille, poids, results_imc, classification, risque))
+
+
+        conn.commit()
+        conn.close()
+        return self.data
+
 
 
 
